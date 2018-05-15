6e7c9895ea93941c4b3bf987fdf32b47a85b2591
robbieve <robbieeve751994@gmail.com>
2018-05-15 10:36:47 +0200

    no message

--- config/default.js
+++ config/default.js
@@ -50,7 +50,7 @@ config = {
         enabled: false,
         save_statistics: true,
     },
-    chatMaxTime: 0.1* 60 * 60 * 1000,
+    chatMaxTime: 2*60*1000, // 8* 60 * 60 * 1000,
     'kue': {
         "cleanupMinutes": 60,
     },
